9e8445aa1f03be9ba118c9f24057803bf107f7f8
praveenpk001 <pk7772094@gmail.com>
2025-04-26 11:41:58 +0530

    Time: 4 ms (97.30%) | Memory: 44.7 MB (82.69%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,25 @@
+class MinStack {
+int min =Integer.MAX_VALUE;
+Stack<Integer> st=new Stack<>();
+    public void push(int val) {
+        if(val<=min){
+            st.push(min);
+            min=val;
+        }
+        st.push(val);
+    }
+    
+    public void pop() {
+        if(st.pop()==min){
+            min=st.pop();
+        }
+    }
+    
+    public int top() {
+       return st.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
