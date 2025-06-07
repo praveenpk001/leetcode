5b608698c92e8d7c316575fb09a3d19433351dba
praveenpk001 <pk7772094@gmail.com>
2025-06-07 17:22:08 +0530

    Time: 20 ms (37.79%) | Memory: 45 MB (63.74%) - LeetSync

---
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+  public int mostFrequentEven(int[] nums) {
+    Map<Integer, Integer> freq = new HashMap<>();
+    int maxCount = 0;
+    int result = -1;
+    for (int n : nums) {
+      if (n % 2 == 0) {
+        int count = freq.getOrDefault(n, 0);
+        freq.put(n, count + 1);
+        if (freq.get(n) > maxCount || (freq.get(n) == maxCount && n < result)) {
+          maxCount = freq.get(n);
+          result = n;
+        }
+      }
+    }
+    return result;
+  }
+}
