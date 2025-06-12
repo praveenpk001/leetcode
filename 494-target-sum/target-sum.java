9a859fbe932242c35f8442f40d3a3143f7dcb931
praveenpk001 <pk7772094@gmail.com>
2025-06-12 12:21:28 +0530

    Time: 632 ms (19.40%) | Memory: 41.7 MB (66.02%) - LeetSync

---
+++ 494-target-sum/target-sum.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        return cal(nums, target, 0, 0);
+    }
+
+    public int cal(int[] nums, int tar, int idx, int currSum) {
+        if (idx == nums.length) {
+            return (currSum == tar) ? 1 : 0;
+        }
+
+        int add = cal(nums, tar, idx + 1, currSum + nums[idx]);
+        int sub = cal(nums, tar, idx + 1, currSum - nums[idx]);
+
+        return add + sub;
+    }
+}
